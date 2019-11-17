62862339dcf830c79d6ea49451a11ca4e2e0feed
1arp <dekhijaegi@gmail.com>
2019-11-17 18:51:37 +0530

    frmated server.js
    Signed-off-by: 1arp <dekhijaegi@gmail.com>

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-const app = require('express')()
+const app = require("express")();
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
 
-app.get('/',(req,res) => {
-    res.send("hello world!")
-})
-
-
-app.listen(PORT)
+app.listen(PORT);
